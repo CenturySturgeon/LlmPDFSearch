5273d96848261d0a7580e764b98d23136b91ad6a
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-11-12 19:02:16 -0600

    Model path loads from env variables

--- llamaTest.py
+++ llamaTest.py
@@ -1,4 +1,9 @@
+import os
+from dotenv import load_dotenv
 from llama_cpp import Llama
-llm = Llama(model_path="./models/mistral-7b-v0.1.Q4_K_M.gguf")
-output = llm("Q: Name the planets in the solar system? A: ", max_tokens=32, stop=["Q:", "\n"], echo=True)
+
+load_dotenv()
+
+llm = Llama(model_path =os.getenv('MODEL_PATH'))
+output = llm("Q: Name the planets in the solar system? A: ", max_tokens=32, echo=True)
 print(output)
