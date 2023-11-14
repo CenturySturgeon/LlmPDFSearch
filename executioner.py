fac54c6f80c2ccbaa44868bdbb391fd054d8352b
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-11-13 19:44:27 -0600

    Executioner can handle multiple commands

--- executioner.py
+++ executioner.py
@@ -1,12 +1,15 @@
 import subprocess
 
-def execute_command(command):
+def execute_python_interactive(commands):
     try:
-        # Run the command and capture the output
-        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        # Construct the command to run Python in interactive mode with the specified commands
+        python_command = '\n'.join(commands) + '\nexit()'
+
+        # Run the Python command and capture the output
+        result = subprocess.run(['python', '-i'], input=python_command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         
         # Print the output
-        print("Command Output:")
+        print("Python Interactive Output:")
         print(result.stdout)
         
         # Print any errors, if present
@@ -15,9 +18,14 @@ def execute_command(command):
             print(result.stderr)
     
     except subprocess.CalledProcessError as e:
-        print(f"Error executing command: {e}")
+        print(f"Error executing Python interactive command: {e}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-# Example: execute 'ls' command
-execute_command('ls')
+# Example: execute multiple commands in Python terminal
+python_commands = [
+    "print('Hello from inside the Python terminal')",
+    "x = 42",
+    "print('The value of x is:', x)"
+]
+execute_python_interactive(python_commands)
