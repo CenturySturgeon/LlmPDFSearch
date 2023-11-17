53d7720f642b00b5e4cc2c726e64e8b34d332a85
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-11-17 08:32:53 -0600

    Pexpect was too troublesome, got the old approach to work in windows

--- winExecutioner.py
+++ winExecutioner.py
@@ -1,13 +1,49 @@
-import pexpect
-from pexpect.popen_spawn import PopenSpawn
+import sys
+import os
+import subprocess
+from subprocess import Popen, PIPE
+import threading
 
-command = 'cmd'
-child = PopenSpawn(command)
-child.expect('>')
-child.sendline('help color')
-child.expect('>')
-output = child.before.decode("cp437")  # Decode the output from bytes to string
-lines = output.splitlines()  # Split the output into separate lines
 
-for line in lines:
-    print(line)
+class LocalShell(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        env = os.environ.copy()
+        self.process = Popen('cmd', stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT, shell=True, env=env)
+        sys.stdout.write("Started Local Terminal...\r\n\r\n")
+
+        def writeall():
+            while True:
+                # print("read data: ")
+                data = self.process.stdout.read(1).decode("cp437")
+                if not data:
+                    break
+                sys.stdout.write(data)
+                sys.stdout.flush()
+
+        writer = threading.Thread(target=writeall)
+        writer.start()
+
+        # try:
+        #     while True:
+        #         d = sys.stdin.read(1)
+        #         if not d:
+        #             break
+        #         self._write(p, d.encode(encoding="cp437"))
+
+        # except EOFError:
+        #     pass
+
+    def _write(self, message):
+        self.process.stdin.write(message)
+        self.process.stdin.flush()
+
+    def kill(self):
+        self.process.kill()
+
+
+shell = LocalShell()
+shell.run()
+shell._write("systeminfo\n".encode())
