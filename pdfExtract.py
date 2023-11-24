de6c10e6eaa6c7ee88a610ccd302cd35ccda61ba
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-11-24 09:13:19 -0600

    Fixed unique id bug, added pdf chunk separator by chars function

--- pdfExtract.py
+++ pdfExtract.py
@@ -1,4 +1,5 @@
 import os
+import chromadb
 import fitz  # PyMuPDF
 
 def extract_pdf_sections(pdf_path):
@@ -26,15 +27,32 @@ def extract_pdf_sections(pdf_path):
         for index, block in enumerate(text):
             documents.append(block[4])
             metadatas.append({"source": document_name, "page": page_number + 1, "paragraph":  index + 1})
-            ids.append("id" + str(idIndex + 1))
+            ids.append("Page: " + str(page_number + 1) + ", Parragraph: " +  str(index + 1))
 
     # Close the PDF file
     pdf_document.close()
 
     return documents, metadatas, ids
 
+def split_pdf_by_chunks(pdf_path, chunk_size=500):
+    pdf_document = fitz.open(pdf_path)
+    chunks_list = []
+
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_num)
+        text = page.get_text("text")
+        chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
+        chunks_list.extend(chunks)
+
+    pdf_document.close()
+    return chunks_list
+
+
 # Replace 'your_pdf_file.pdf' with the path to your PDF file
 pdf_path = 'TheRoadNotTaken.pdf'
 docs, mets, ids = extract_pdf_sections(pdf_path)
 
 print(len(docs), len(mets), len(ids))
+
+chunks = split_pdf_by_chunks('TheRoadNotTaken.pdf',1200)
+print(chunks[0])
